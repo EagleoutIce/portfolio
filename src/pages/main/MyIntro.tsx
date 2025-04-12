ed310e55ac5074582572a3e7173c664f81f9aab2
Florian Sihler <florian.sihler@uni-ulm.de>
2025-04-12 11:08:31 +0200

    refactor: minor tune to title sizes

--- src/pages/main/MyIntro.tsx
+++ src/pages/main/MyIntro.tsx
@@ -2,8 +2,8 @@ import "./MyIntro.css";
 
 export function MyIntro() {
    return <div className="main">
-      Hey and welcome on my little page! 
-      I am Florian and currently work on my PhD at Ulm University (since September 2023). My primary research interests are in the area of Hybrid Program Analysis to support Program Comprehension.
+      Hey and welcome to my little page! 
+      My name is Florian and I am currently working on my PhD at Ulm University (since September 2023). My primary research interests are in the area of Hybrid Program Analysis to support Program Comprehension.
       My work received the <a href="https://www.uni-ulm.de/in/sp/institute/news-detail/article/youngrse-award-at-derse24/" target="_blank" rel="noreferrer">YoungRSE</a> award at the deRSE24 and the <a href="https://www.uni-ulm.de/in/sp/institute/news-detail/article/award-for-the-best-masters-degree/" target="_blank" rel="noreferrer">award for the best master's degree</a> in the field of computer science at Ulm University.
       <br />
       
