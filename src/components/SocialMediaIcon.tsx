46cc311e3719d29131bf4a71cb1af401b3a160a3
Florian Sihler <florian.sihler@uni-ulm.de>
2025-04-19 23:22:58 +0200

    refactor: note de

--- src/components/SocialMediaIcon.tsx
+++ src/components/SocialMediaIcon.tsx
@@ -9,11 +9,12 @@ interface SocialMediaIconProps {
    readonly href?: string;
    readonly id?: string;
    readonly className?: string;
+   readonly suffix?: string;
 }
 
-export function SocialMediaIcon({ icon, href, id, className }: SocialMediaIconProps) {
+export function SocialMediaIcon({ icon, href, id, className, suffix }: SocialMediaIconProps) {
     return href ? <a href={href} target="_blank" rel="noreferrer" className={`small-media-icon ${className ?? ''}`} id={id}>
-         <FontAwesomeIcon icon={icon} />  
-      </a> : <FontAwesomeIcon icon={icon} className={`small-media-icon ${className ?? ''}`} id={id} />;
+         <FontAwesomeIcon icon={icon} /> {suffix}
+      </a> : <span className={`small-media-icon ${className ?? ''}`} ><FontAwesomeIcon icon={icon} id={id} />{suffix}</span>;
 }
        
