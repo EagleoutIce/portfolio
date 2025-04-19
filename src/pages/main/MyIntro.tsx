19f694f502838aa51de05a8cfb6d2f985a068287
Florian Sihler <florian.sihler@uni-ulm.de>
2025-04-19 23:29:08 +0200

    refactor: add reference

--- src/pages/main/MyIntro.tsx
+++ src/pages/main/MyIntro.tsx
@@ -5,9 +5,9 @@ export function MyIntro() {
       Hey and welcome to my little page! 
       My name is Florian and I am currently working on my PhD at Ulm University (since September 2023). My primary research interests are in the area of Hybrid Program Analysis to support Program Comprehension.
       My work received the <a href="https://www.uni-ulm.de/in/sp/institute/news-detail/article/youngrse-award-at-derse24/" target="_blank" rel="noreferrer">YoungRSE</a> award at the deRSE24 and the <a href="https://www.uni-ulm.de/in/sp/institute/news-detail/article/award-for-the-best-masters-degree/" target="_blank" rel="noreferrer">award for the best master's degree</a> in the field of computer science at Ulm University.
-      <br />
+      <br /><br />
       
       If you want to know more about my work, please check out the projects below.
-      
+      In case you are interested in open theses and projects regarding my work at Ulm University, please have a look at the appropriate <a href="https://www.uni-ulm.de/in/sp/team/florian-sihler/#c978079" target="_blank" rel="noreferrer">section on my institute website</a>.
    </div>
 }
