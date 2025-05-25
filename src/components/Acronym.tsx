34a52bd0b9c10d318178593bee46eb24e89b3c18
Florian Sihler <florian.sihler@uni-ulm.de>
2025-05-25 11:44:58 +0200

    feat: add bachelor seminar on waddle

--- src/components/Acronym.tsx
+++ src/components/Acronym.tsx
@@ -1,14 +1,14 @@
 import { Tooltip } from "react-tooltip";
 import { escapeId } from "../util/id";
 
-interface AcronymProps {
+interface ShortLongProps {
    readonly short: string | JSX.Element;
    readonly long: string;
    readonly id?: string
 }
 
-export default function Acronym({ short, long, id = long }: AcronymProps) {
-   const idKey = 'acronym-' + escapeId(id);
+export default function ShortLong({ short, long, id = long }: ShortLongProps) {
+   const idKey = 'short-long-' + escapeId(id);
    return <>
       <span id={idKey} style={{ cursor: 'help' }}>{short}</span>
       <Tooltip anchorSelect={`#${idKey}`} content={long} key={`tt-${idKey}`} place="bottom" style={{ padding: '2px 6px', margin: '-6px 0px' }}/>
