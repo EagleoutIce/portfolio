6c778c9ac8defa12261cbe0fc6b4d4d1d4890d46
EagleoutIce <florian.sihler@uni-ulm.de>
2025-09-17 21:56:46 +0200

    feat: adding new theses (closes #115)

--- src/pages/main/ThesesData.tsx
+++ src/pages/main/ThesesData.tsx
@@ -118,6 +118,77 @@ We first identify relevant data frame operations by analyzing the most commonly 
 The supported features of our concept and implementation are limited, as there are many different data frame functions in R with many edge cases. Moreover, there is no formal definition of the operational semantics of R and most semantics are defined implicitly. As the scope of this thesis is limited, we focus on a subset of data frame operations identified based on the most common data frame functions in R scripts and R packages. Our implementation was correct for all points of interest of the evaluated labelled R scripts. We inferred data frame shapes for 73.4&nbsp;% of the evaluated points of interest that represent data frames. Of the total number of 33&nbsp;314&nbsp;real-world R scripts, our implementation inferred data frames shapes for 66.9&nbsp;% of all scripts. In total, we inferred data frame shape constraints for 406&nbsp;890 data frame expressions with partial shape information for 34.1&nbsp;% and full shape information for 2.6&nbsp;% of the data frame expressions. Our implementation required, on average, 4.16&nbsp;s with high outliers and a median of 209&nbsp;ms. The average runtime of the total analysis, including the parsing, data flow graph construction, and control flow graph generation, was 4.9&nbsp;s with a median of 598&nbsp;ms. We consider the median total runtime of 598&nbsp;ms to be fast enough for most use cases.</>,
    examiners: ['mtt', 'rh'],
 })
+theses.push({
+   author: 'Julian Schubert',
+   type: 'bachelor-thesis',
+   year: 2025,
+   month: 8,
+   title: 'Designing a Query Language for Linting R Scripts to Improve Reproducibility',
+   abstract: <>In this bachelor’s thesis, we develop a query language which allows
+analyzing semantic information extracted from R scripts. We create an
+extendable linting framework with the help of our query language and
+implement and evaluate the performance of four reproducibility-focused
+linting rules.
+<p/>
+The R programming language is frequently used in data science research,
+where R scripts are included as part of reproducibility packages to replicate
+researchers’ findings. In many cases, such scripts fail to complete without
+errors or do not replicate the findings described in their accompanying
+research. In addition, there is a lack of tools that support developers in the
+creation of reproducible R scripts.
+<p/>
+We describe and implement a query language which can be used to implement reproducibility-focused linting rules as part of flowR. Specifically, we
+select common reproducibility issues, which we remedy through a set of
+linting rules created with the help of the query language. We then evaluate
+the linter based on multiple criteria on a set of real-world R scripts.
+<p/>
+We propose and implement a set of query language operations in the form
+of query generators and subsequent transformers. Further, we implement
+a linting framework that uses the query language to detect and output
+issues in R scripts. Using the linting framework, we implement four linting
+rules for reproducibility issues inspired by related tools and frequently
+found in the literature. Then, we evaluate the correctness and performance
+of the query language and our linting rules through 341&nbsp;unit tests as well
+as a combination of automatic performance measurements and manual
+spot-checks on an extensive dataset of real-world R&nbsp;scripts.
+<p/>
+This work focuses on the implementation and validity testing of the query
+language, linting framework and our rules, and does not account for
+whether the rules are considered helpful to users. Additionally, our linter’s
+usability is limited to commandline interaction and does not yet expose
+editor functionality.
+<p/>
+We find that our query language is practical for the creation of complex
+linting rules based on a combination of simple query operations. Executing
+our linting framework on a dataset of 35&nbsp;373 real-world R scripts, we find
+that our linting rules return frequent matches in practice, with only 13&nbsp;%
+of files containing no match. Our evaluation additionally shows that our
+linter requires a median time of 695&nbsp;ms, with the average time of 7&nbsp;259&nbsp;ms
+being heavily affected by outliers in the form of very large scripts in our
+dataset.</>,
+   examiners: ['mtt'],
+})
+theses.push({
+   author: 'Simon Martin',
+   type: 'bachelor-thesis',
+   year: 2025,
+   month: 8,
+   title: 'Improving Linting for Semantic Errors in the Data Science Domain With the Help of Large Language Models',
+   abstract: <>In this bachelor’s thesis, we explore the use of Large Language Models&nbsp;(LLMs) to search for semantic errors in a given R&nbsp;file. We aim to investigate the effectiveness of LLMs in linting for semantic errors and examine their advantages and disadvantages. 
+<p/>
+In today’s digitalized world, programming is becoming increasingly essential, especially for non-domain experts. Researchers in different kinds of study areas need to analyse their data reliably. Therefore, they create their own programs. The development process of these programs can be supported by various tools, such as linters, to identify errors in the created programs. Yet, creating such linters is challenging and requires a lot of work, especially for problems that require a semantic understanding of a specific domain. We aim to investigate whether LLMs can narrow this gap by finding these errors without requiring complex linter rules.
+<p/>
+Our goal is to construct a model that can identify what we call high level error types, like a wrong statistical test method error, which are too complex for typical static analysis tools. We want to achieve this by prompting a LLM to find them. The advantage of this method is that we can simply explain the error in natural language and tell the LLM to search for it. Therefore, we do not need to create a complex linter rule, which requires a lot of work, such as analyzing the structure of data frames.
+<p/>
+To analyze the capabilities of this method, we first conduct a prestudy to determine the optimal setup for prompting an LLM to identify errors in an R file. In addition to the high level error types, the model should identify what we call low level error types, which can be detected by typical static analysers. As ground truth for these errors, we use lintr. To analyse the model, we let it search for three low level and two high level error types in a total of about 12&nbsp;500&nbsp;randomly selected R&nbsp;files from a dataset of more than 33&nbsp;000&nbsp;files. The found low level errors are then automatically compared to found instances by the R&nbsp;linter lintr, whereas the high level ones are compared to placed errors in modified files. Moreover, a subset
+of the found errors is checked manually. 
+<p/>
+In the described analysis, we only test the models ’s capabilities for the five selected error types and only for the programming language R. We do not know whether the setup works similarly for other error types and programming languages.
+<p/>
+We observe that the capabilities of our created model, which utilizes zero-shot learning, and a code snippet of 50&nbsp;lines as the context size, are limited. We compare its performance, when using Llama3.1:70b and ChatGPT 4o-mini as LLM; however we do not see too big differences. For the evaluation, we use a ground truth of automatically placed errors with a modified version of mutation testing, and found errors by lintr. The comparison reveals, that our model does not even find every 10th&nbsp;error, which we consider to be insufficient. Its precision is better; however, compared to other static analysers the results are again not satisfactory. However, not all error types perform this poorly. We observe a trend where low level error types perform worse than the high level ones, and conclude that our created model is not a possible replacement for lintr. For high level error types that perform well, however, it can be a useful tool, especially since no other tool is known to us that can identify them. Therefore, a hybrid approach to find all error types could be a successful solution.
+</>,
+   examiners: ['mtt'],
+})
 
 export function getTheses(type: 'master' | 'bachelor'): JSX.Element[] {
    return theses
@@ -127,7 +198,7 @@ export function getTheses(type: 'master' | 'bachelor'): JSX.Element[] {
    )
    .map(({ title, author, examiners, abstract, link, year, month}) => {
       const id = escapeId(title);
-      return <><li key={id}>
+      return <li key={id}>
          <strong id={'link-' + id}>{title} <span className='theses-author-meta'>({author !== 'anonymous' ? author + ', ' : ''}{monthToString[month - 1]}&nbsp;{year})</span></strong><br />
          <span>Examiners: {joinLastWith(examiners.map(e => ExaminerMap[e]))}</span>
          <details style={{ margin: '0.5em 0 1em 0' }}>
@@ -136,8 +207,7 @@ export function getTheses(type: 'master' | 'bachelor'): JSX.Element[] {
          </details>
          
          {link && <a href={link} target="_blank" rel="noreferrer">Link to Thesis</a>}
-      </li>
-      </>;
+      </li>;
    });
 }
 
