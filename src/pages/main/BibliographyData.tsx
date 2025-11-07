389f590b89d4aa67931d377424d02ccb2b65a03b
Florian Sihler <florian.sihler@uni-ulm.de>
2025-11-07 15:59:50 +0100

    refactor: rename poster.bib => posters.bib

--- src/pages/main/BibliographyData.tsx
+++ src/pages/main/BibliographyData.tsx
@@ -1,4 +1,4 @@
 export { default as BibDataMain } from '../../resources/bibliographies/publications.bib?raw';
 export { default as BibDataTalks } from '../../resources/bibliographies/talks.bib?raw';
-export { default as BibDataPoster } from '../../resources/bibliographies/poster.bib?raw'
+export { default as BibDataPoster } from '../../resources/bibliographies/posters.bib?raw'
 export { default as BibDataOther } from '../../resources/bibliographies/other.bib?raw';
