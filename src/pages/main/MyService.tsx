e1aaa7726e65f8a6ed1fdacb105686a907658355
Florian Sihler <florian.sihler@uni-ulm.de>
2026-03-25 11:49:37 +0100

    refactor: refine intro text

--- src/pages/main/MyService.tsx
+++ src/pages/main/MyService.tsx
@@ -3,7 +3,7 @@ import { getService } from './ServiceData';
 
 export function MyService() {
   return <>
-   I served as reviewer or artifact evaluator for the following conferences:
+   I served as chair, reviewer or artifact evaluator for the following conferences:
    <ul className='seminars-list'>
       {getService()}
    </ul>
